a4b01c63929f0cbfe6c17735bee3950b13baf1e6
Guilherme Assunção <cgmatosassuncao@gmail.com>
2024-01-26 10:36:35 -0300

    Update

---
+++ resources/views/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('title', 'Adicionar tarefa')
+
+@section('styles')
+    <style>
+        .error-message {
+            color: red;
+            font-size: 0.8rem;
+        }
+    </style>
+@endsection
+
+@section('content')
+    <form action="{{ route('tasks.store') }}" method="POST" >
+        @csrf
+        <div>
+            <label for="title">Título</label>
+            <input type="text" name="title" id="title" value="{{old('title')}}">
+            @error('title')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <label for="description">Descrição</label>
+            <textarea name="description" id="description" rows="5">{{old('description')}}</textarea>
+            @error('description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <label for="long_description">Descrição Longa</label>
+            <textarea name="long_description" id="long_description" rows="10">{{old('long_description')}}</textarea>
+            @error('long_description')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <button type="submit">Adicionar tarefa</button>
+        </div>
+    </form>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de tarefas</title>
+
+    @yield('styles')
 </head>
 <body>
     <h1>@yield('title')</h1>
-    <div>@yield('content')</div>
+    <div>
+        @if (session()->has('success'))
+            <div>{{ session('success') }}</div>
+        @endif
+
+        @yield('content')
+    </div>
 </body>
 </html>
